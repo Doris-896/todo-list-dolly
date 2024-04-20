c4efaa8370da12d5c58de12044a8a1abb1a2b33c
Teo <dolly@gmail.com>
2024-04-20 18:15:09 +0700

    Add readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import ButtonUsage from "@/components/Button";
+import Image from "next/image";
 
-export default function Page() {
+export default function Home() {
   return (
-    <>
-      <ButtonUsage />
-    </>
+    <main className="w-full h-full">
+      <h1>Hello world</h1>
+    </main>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -2,5 +2,9 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 
 export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <Button variant="contained" className="text-gray-700">
+      Hello world
+    </Button>
+  );
 }
